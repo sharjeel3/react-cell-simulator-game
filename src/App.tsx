f723a7862129e533664aab8e209043c190e3a190
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-10 22:59:55 +1100

    Add global styles for reset and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { globleStyles } from './ui-library/global';
+
+const GlobalStyles = createGlobalStyle`
+  ${globleStyles}
+`;
 
 const App: React.FC = () => {
-  return <div>test</div>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
---
+++ src/ui-library/brand/colors.tsx
@@ -0,0 +1,4 @@
+export const colors = {
+  grey: '#aaaaaa',
+  green: 'green'
+};
